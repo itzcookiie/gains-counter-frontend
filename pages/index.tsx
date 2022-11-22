753962533e8acd2b5fb9df220fb4fc9aad6755da
Mayowa <38388654+itzcookiie@users.noreply.github.com>
2022-11-22 23:16:09 +0000

    Start of project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { useCallback, useEffect, useState } from 'react'
+import styles from '../styles/home.module.scss'
+
+function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Fast Refresh Demo</h1>
+    </main>
+  )
+}
+
+export default Home
