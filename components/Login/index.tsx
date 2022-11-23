ecd6b4595b076d4f051c597b1b135ec69f41894a
Mayowa <38388654+itzcookiie@users.noreply.github.com>
2022-11-23 23:37:07 +0000

    Create basic login component

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,3 +1,9 @@
 export default function Login() {
-    
+    return (
+        <>
+          <h3>Enter your username:</h3>
+          <input name="username" required />
+          <p>If you do not have an account already, create one automatically by entering a chosen username</p>
+        </>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import { useCallback, useEffect, useState } from 'react'
 import styles from '../styles/home.module.scss'
+import Login from '../components/Login/index';
 
 function Home() {
   return (
     <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
+      <div>        
+        <section>
+          <img />
+        </section>
+        <section>
+          <h1>Gains counter</h1>
+          <Login />
+        </section>
+      </div>
     </main>
   )
 }
